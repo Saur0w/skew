14e5d8e6cc290e3b567fe025fd77ef5c06829391
Saur0w <sthapliyal085@gmail.com>
2026-09-25 13:04:37 +0530

    "Structure"

---
+++ src/components/Landing/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "./styles.module.css";
+
+export default function Landing() {
+    return (
+        <div className={styles.landing}>
+
+        </div>
+    )
+}
